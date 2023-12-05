414d5ebcc4b0d8beac082f208ca28c64fb17a6f0
pedroPecly <p.pecly@gsuite.iff.edu.br>
2023-12-05 00:10:48 -0300

    atualizando

--- src/main/java/controller/ConnFactory.java
+++ src/main/java/controller/ConnFactory.java
@@ -6,8 +6,8 @@ import javax.persistence.Persistence;
 
 public class ConnFactory {
     public static EntityManager getEntityManager(){
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("TestePU");
-        EntityManager entity = factory.createEntityManager();
-        return entity;
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("AgendaPU");
+        EntityManager entityManager = factory.createEntityManager();
+        return entityManager;
     }
 }
--- src/main/java/view/Principal.java
+++ src/main/java/view/Principal.java
@@ -2,17 +2,43 @@ package view;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import controller.ConnFactory;
-import model.Ovo;
+import model.Contato;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Ovo ovo = new Ovo("caipiria2", 21.89);
+        Contato c = new Contato("ovo", "12345678", "ovo.com");
+
         EntityManager em = ConnFactory.getEntityManager();
 
         em.getTransaction().begin();
-        em.persist(ovo);
+        /*
+            //inclusao
+            em.persist(c);
+        */
+
+        /*
+            //ediçao
+            c.setId(2);
+            c.setNome("ovo alterado");
+            em.merge(c);
+        */
+        
+        /*
+            //exclusao
+            em.remove(em.find(Contato.class, 1)); //em.find para buscar
+        */
+
+        //listar
+        List<Contato> lista = em.createQuery("FROM Contato c").getResultList();
+        for (Contato c1 : lista) {
+            System.out.println(c1);
+        }
+
+        // enviando todas as ordens necessarias
         em.getTransaction().commit();
         em.close();
     }
