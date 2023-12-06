e60a0b24d667d3f1036796c9c548b5eec1d487b9
pedroPecly <p.pecly@gsuite.iff.edu.br>
2023-12-05 21:43:09 -0300

    no message

--- src/main/java/view/Principal.java
+++ src/main/java/view/Principal.java
@@ -1,44 +1,11 @@
 package view;
 
-import javax.persistence.EntityManager;
-
-import java.util.List;
-
 import model.Contato;
-import model.dao.ConnFactory;
 
 public class Principal {
 
     public static void main(String[] args) {
         Contato c = new Contato("ovo", "12345678", "ovo.com");
-
-        /*
         
-            EntityManager em = ConnFactory.getEntityManager();
-
-            em.getTransaction().begin();
-
-            // inclusao
-            em.persist(c);
-
-            // ediçao
-            c.setId(2);
-            c.setNome("ovo alterado");
-            em.merge(c);
-
-            // exclusao
-            em.remove(em.find(Contato.class, 1)); // em.find para buscar
-
-            // listar
-            List<Contato> lista = em.createQuery("FROM Contato c").getResultList();
-            for (Contato c1 : lista) {
-                System.out.println(c1);
-            }
-
-            // enviando todas as ordens necessarias
-            em.getTransaction().commit();
-            em.close();
-
-        */
     }
 }
